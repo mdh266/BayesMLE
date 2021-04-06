f33d82d9c3ab1ec1aa93ecad6ceeb49144b0608f
Mike Harmon <mdh266@gmail.com>
2021-04-05 20:26:00 -0400

    Adding more write up

--- src/main/scala/PoissonEntryPoint.scala
+++ src/main/scala/PoissonEntryPoint.scala
@@ -3,9 +3,7 @@ package com.github.mdh266
 case class PoissonEntryPoint() {
 
 	def Poisson(lambda : Double) : PoissonDistribution = {
-		var p = new PoissonDistribution()
-		p.setLambda(lambda)
-		p
+		new PoissonDistribution(lambda)
 	}
 
 }
