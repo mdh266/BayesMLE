25fd0cc31f27c8a9b78b4f2134621d7d6d67b443
Mike Harmon <mdh266@gmail.com>
2021-05-22 18:07:09 -0400

    updating server start up

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,16 @@
 package com.github.mdh266
 
+import java.net.InetAddress
 import py4j.GatewayServer
 
 object Main {
 	def main(args: Array[String]) = {
-		val server = new GatewayServer(new PoissonEntryPoint())
-		server.start()
-		System.out.println("Gateway Server Started")
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		val addr = InetAddress.getByName("0.0.0.0")
+		val app = new PoissonEntryPoint()
+		val builder = new GatewayServer.GatewayServerBuilder(app)
+		builder.javaAddress(addr);
+    val server = builder.build();
+		println("Gateway Server Started")
 	}
 }
